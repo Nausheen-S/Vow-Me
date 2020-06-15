36aa1fd8f4cabca1e9fac086e1e20c83c9501494
Nausheen_S <salauddinnausheen@gmail.com>
2020-06-15 16:04:03 +0800

    made migration change for heroku

--- db/migrate/20200609132337_create_products.rb
+++ db/migrate/20200609132337_create_products.rb
@@ -1,5 +1,12 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
+    create_table :categories do |t|
+      t.string :name
+
+
+
+      t.timestamps
+    end
     create_table :products do |t|
       t.string :name
       t.text :description
